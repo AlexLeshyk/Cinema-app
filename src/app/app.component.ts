a93d0db99e1dd2469e00eab4a480bd920f49d1da
AlexLeshyk <gashy_jet@yahoo.com>
2020-03-16 18:25:54 +0300

    server file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'cinema-app';
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get('https://www.themoviedb.org/discover/movie?sort_by=popularity.desc')
+      .subscribe( response => {
+        console.log('Response', response)
+      })
+  }
 }
